cc3ef2f107ddfd84504e3f7b7afc1cd031a2d7ac
ryanwall5 <ryanwall5@yahoo.com>
2016-10-01 10:36:05 -0700

    lab1 1 commit

---
+++ Lab1/Lab1/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using Lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            Person fred = new Person();
+            fred.LastName = "wall";
+
+            ViewBag.FirstName = "Ryan";
+            ViewBag.LastName = fred.LastName;
+            ViewBag.Address = "815 awesome st. Canby, Oregon 97013";
+            return View();
+        }
+        
+    }
+}
